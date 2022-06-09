3530b95798e27d6d5d3ea84eb095b555c91f0542
Luca Carducci <luca.carducci91@gmail.com>
2022-06-09 15:17:14 +0200

    test

--- app.js
+++ app.js
@@ -7,43 +7,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const sendRequest = async function (req, res, next) {
-  console.log(req.body);
+  try {
+    console.log(req.body);
 
-  const webhookUrl = "https://hooks.slack.com/services/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";
-  const testUrl = "https://hooks.slack.com/services/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";
-  await axios.post(testUrl, {
-    blocks: [
-      {
-        type: "header",
-        text: {
-          type: "plain_text",
-          text: "New deploy request",
-          emoji: true,
+    const webhookUrl = "https://hooks.slack.com/services/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";
+    const testUrl = "https://hooks.slack.com/services/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";
+    await axios.post(testUrl, {
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "New deploy request",
+            emoji: true,
+          },
         },
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: " ",
-        },
-        accessory: {
-          type: "checkboxes",
-          options: [
-            {
-              text: {
-                type: "mrkdwn",
-                text: "Deploy completed",
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: " ",
+          },
+          accessory: {
+            type: "checkboxes",
+            options: [
+              {
+                text: {
+                  type: "mrkdwn",
+                  text: "Deploy completed",
+                },
+                value: "value-0",
               },
-              value: "value-0",
-            },
-          ],
-          action_id: "checkboxes-action",
+            ],
+            action_id: "checkboxes-action",
+          },
         },
-      },
-    ],
-  });
-  res.json({ title: "Express" });
+      ],
+    });
+    res.json({ result: "ok" });
+  } catch (e) {
+    console.error(e);
+    res.json(500, { error: e });
+  }
 };
 
 app.post("/sendRequest", sendRequest);
